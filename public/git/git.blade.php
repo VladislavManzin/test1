a8c3f25683b67cf6561a0a97474409c12811e8d7
VladislavManzin <ystipion02@gmail.com>
2022-11-18 01:22:21 -0800

    О нас, Каталог и Где найти нас.

---
+++ public/git/git.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Funny Comics Land</title>
+    <link rel="stylesheet" href="git.css">
+</head>
+<body>
+    <div>
+        <a href="AboutUs.blade.php">О нас</a>
+        <a href="catalog.blade.php">Каталог</a>
+        <a href="WhereToFindUs.blade.php">Где нас найти</a>
+    </div>
+</body>
+</html>
